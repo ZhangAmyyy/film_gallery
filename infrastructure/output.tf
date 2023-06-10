272a618c43d297ed04dac8e737cad9cdac5b557d
ZhangAmyyy <zhangyy_amy@outlook.com>
2023-06-09 17:39:24 -0700

    install gcloud sdk

--- infrastructure/output.tf
+++
@@ -1,6 +0,0 @@
-output "film_bucket_name" {
-  value = aws_s3_bucket.film_s3_bucket.id
-}
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.s3_distribution.id
-}
--- src/redux/actions/routes.js
+++
@@ -1,9 +0,0 @@
-import { APP_ROUTES, PATH_URL } from '../types';
-
-export const appRoutes = (routes) => async (dispatch) => {
-  dispatch({ type: APP_ROUTES, payload: routes });
-};
-
-export const pathURL = (path, url) => async (dispatch) => {
-  dispatch({ type: PATH_URL, payload: { path, url } });
-};
